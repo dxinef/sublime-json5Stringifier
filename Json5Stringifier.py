2c3e65cfcfabddf40664d1cb573e0376d9d5f524
dxinef <7739224@qq.com>
2019-10-20 15:20:55 +0800

    修改bug

--- Json5Stringifier.py
+++ Json5Stringifier.py
@@ -6,30 +6,28 @@ from subprocess import Popen, PIPE, STDOUT
 
 class Json5StringifierCommand (sublime_plugin.TextCommand):
   def run (self, edit):
-    pkgName = 'dxinefJson5Stringifier'
-
     view = self.view
     window = view.window()
-    window.destroy_output_panel(pkgName + '_errOutput')
-    # 获取选择区域的文字内容。如没有选区，默认为全文
+    window.destroy_output_panel('Json5Stringifier_errOutput')
+
     region = view.sel()[0]
     if (region.size() == 0):
       region = sublime.Region(0, view.size())
     text = view.substr(region).encode("utf-8")
-    # 发送到shell命令
-    pkgPath = os.path.join( sublime.packages_path(), pkgName, 'node_modules', '.bin')
-    os.chdir(pkgPath)
+
+    pkgDir = os.path.dirname(__file__)
+    json5CliPath = os.path.join( pkgDir, 'node_modules', '.bin')
+    os.chdir(json5CliPath)
+
     cmd = Popen(['json5', '-s', '2'], shell=True, stdout=PIPE, stdin=PIPE, stderr=PIPE)
     result, err = cmd.communicate(input=text)
 
     if err:
       outputPanel = window.create_output_panel(pkgName + '_errOutput')
       outputPanel.insert(edit, 0, err.decode("utf-8"))
-      window.run_command('show_panel', { 'panel': 'output.' + pkgName + '_errOutput' })
+      window.run_command('show_panel', { 'panel': 'output.Json5Stringifier_errOutput' })
     else:
       outputPanel = window.new_file();
       outputPanel.set_name('output.json')
       outputPanel.assign_syntax('Packages/JavaScript/JSON.sublime-syntax')
       outputPanel.insert(edit, 0, result.decode("utf-8"))
-
-
